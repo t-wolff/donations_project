477caf84f52b6519e7c7742a3580f47bb5edd047
Tehilla Wolf <tehillawolf@Tehillas-MacBook-Pro.local>
2023-11-23 08:59:47 +0200

    ui donate

--- src/pages/donate/Donate.jsx
+++ src/pages/donate/Donate.jsx
@@ -4,56 +4,93 @@ import { usePayPalScriptReducer, PayPalButtons } from "@paypal/react-paypal-js";
 import { Input } from "../../components";
 
 const Donate = () => {
-    const [{ isPending, scriptError }] = usePayPalScriptReducer();
-    const [loading, setLoading] = useState(true);
+  const [{ isPending, scriptError }] = usePayPalScriptReducer();
+  const [loading, setLoading] = useState(true);
+  const [amount, setAmount] = useState(1);
+  const [name, setName] = useState(0);
 
-    useEffect(() => {
-        if (!isPending) {
-            setLoading(false);
-        }
-    }, [isPending]);
+  useEffect(() => {
+    if (!isPending) {
+      setLoading(false);
+    }
+  }, [isPending]);
 
-    const createOrder = (data, actions) => {
-        return actions.order.create({
-            purchase_units: [
-                {
-                    amount: {
-                        value: "1.00",
-                        currency_code: "USD",
-                    },
-                },
-            ],
-        });
-    };
+  useEffect(() => {
+    console.log("Amount:", amount);
+    console.log("Name:", name);
+  }, [amount, name]);
+  
 
-    const onApprove = (data, actions) => {
-        return actions.order.capture().then(function (details) {
-            alert("Transaction completed by " + details.payer.name.given_name);
-        });
-    };
+  const createOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            value: amount,
+            currency_code: "USD",
+          },
+        },
+      ],
+    });
+  };
 
-    const onError = (err) => {
-        console.error("PayPal Error:", err);
-    };
+  const onApprove = (data, actions) => {
+    return actions.order.capture().then(function (details) {
+      alert("Transaction completed by " + details.payer.name.given_name);
+    });
+  };
 
-    return (
-        <div className="donate-container">
-            {loading && <div className="loading-spinner">Loading...</div>}
-            {scriptError && <div className="error-message">Error loading PayPal script</div>}
-            {!loading && !scriptError && (
-                <>
-                <Input className='paypal-btn'/>
-                <PayPalButtons
-                    createOrder={createOrder}
-                    onApprove={onApprove}
-                    onError={onError}
-                    className='paypal-btn'
-                    disabled={isPending}
-                />
-                </>
-            )}
-        </div>
-    );
+  const onError = (err) => {
+    console.error("PayPal Error:", err);
+  };
+
+  return (
+    <div className="donate-container">
+      <aside>
+        <h2>Donate Now</h2>
+        <h4>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia est
+          ullam alias modi! Eligendi ratione nostrum tempore perferendis
+          asperiores quidem.
+        </h4>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia est
+          ullam alias modi! Eligendi ratione nostrum tempore perferendis
+          asperiores quidem.
+        </p>
+      </aside>
+      <div className="payment-container">
+        {loading && <div className="loading-spinner">Loading...</div>}
+        {scriptError && (
+          <div className="error-message">Error loading PayPal script</div>
+        )}
+        {!loading && !scriptError && (
+          <>
+            <label htmlFor="name">Name:</label>
+            <input
+              name="name"
+              type="text"
+              placeholder="John Doe"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <h2 htmlFor="amount">My Generous Donation Of:</h2>
+            <div className="btn-amount-container">
+            <button onClick={() => setAmount(50)}>50$</button>
+            <button onClick={() => setAmount(100)}>100$</button>
+            <button onClick={() => setAmount(300)}>300$</button>
+            </div>
+            <PayPalButtons
+              createOrder={createOrder}
+              onApprove={onApprove}
+              onError={onError}
+              className="paypal-btn"
+              disabled={isPending}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default Donate;
